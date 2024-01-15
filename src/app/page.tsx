66778174a2c0db25786686d51e37700e31fe8154
raphauy <rapha.uy@rapha.uy>
2024-01-15 12:29:55 -0300

    First commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
 import { Button } from '@/components/ui/button'
+import { getCurrentUser } from '@/lib/auth'
+import { getClientDAO } from '@/services/client-services'
 import Image from 'next/image'
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+
+  const currentUser= await getCurrentUser()
+  if (!currentUser) return <div>User not found</div>
+
+  if (currentUser.role === 'admin') {
+    redirect('/admin')
+  }else {
+    const client= await getClientDAO(currentUser.clientId)
+    const slug= client.slug
+    redirect(`/${slug}`)
+  }
   return (
     <div className='flex flex-col items-center gap-10 mt-10'>
       <h1 className='text-2xl font-bold'>Page</h1>
